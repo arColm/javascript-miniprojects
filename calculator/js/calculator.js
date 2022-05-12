e083582389bcbf4a229e09fd19195415207c08f8
pokerdf <jacob.leung@mail.mcgill.ca>
2022-05-11 23:03:56 -0400

    added disfunctional UI

---
+++ calculator/js/calculator.js
@@ -0,0 +1,16 @@
+
+
+
+
+const buttons = document.querySelectorAll("button");
+
+let currentNum = "";
+
+buttons.forEach(button => {
+    button.addEventListener("click", e=> {
+        console.log(e.target.class);
+        switch(e.target.class) {
+
+        }
+    })
+})
