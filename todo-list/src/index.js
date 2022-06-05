8b972a7a9a8138d7fa95461ce2e8202f593c8df3
pokerdf <jacob.leung@mail.mcgill.ca>
2022-06-05 02:01:11 -0400

    added minimal styling

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -1,21 +1,6 @@
 
 import * as DOM from "./DOM.js";
 
-//Remove this line after finishing
-import * as project from "./project.js";
-
-//Remove this section after finishing
-const content = document.querySelector("#content");
-const sidebar = document.querySelector("#sidebar");
-const header = document.querySelector("#header");
-const popup = document.querySelector("#popup");
-const popupBackground = document.querySelector("#popup-background");
-const todoList = document.querySelector("#todo-list");
-//
-
-
-let currentProject = "";
-
 DOM.renderProjectList();
 DOM.renderHeader();
 DOM.renderTasks("testProject");
