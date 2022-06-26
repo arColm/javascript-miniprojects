ddd897ab1b6e0ee28dea8222ed3e809d42e10e7e
pokerdf <jacob.leung@mail.mcgill.ca>
2022-06-26 17:41:26 -0400

    added refresh board function to DOM.js

--- battleship/src/DOM.js
+++ battleship/src/DOM.js
@@ -3,6 +3,30 @@ import * as Gameboard from "./gameboard.js";
 const contentDiv = document.querySelector("#content");
 const BOARDSIZE = 6;
 
+class Game {
+    constructor(playerBoard,playerBoardElement,computerBoard,computerBoardElement) {
+        this.playerBoard=playerBoard;
+        this.playerBoardElement=playerBoardElement;
+        this.computerBoard=computerBoard;
+        this.computerBoardElement=computerBoardElement;
+    }
+
+    getPlayerBoard() {
+        return this.playerBoard;
+    }
+
+    getPlayerBoardElement() {
+        return this.playerBoardElement;
+    }
+
+    getComputerBoard() {
+        return this.computerBoard;
+    }
+
+    getComputerBoardElement() {
+        return this.computerBoardElement;
+    }
+}
 
 function createGameboardElement(size) {
     const gameboard = Gameboard.createGameboard(size);
@@ -15,6 +39,7 @@ function createGameboardElement(size) {
         for(let column=0;column<size;column++) {
             let td = document.createElement("td");
             tr.appendChild(td);
+            td.setAttribute("class","empty unhit");
         }
         table.appendChild(tr);
     }
@@ -24,18 +49,72 @@ function createGameboardElement(size) {
     return gameboardDiv;
 }
 
-
+/**
+ * This function takes a game object, and a player and refreshes that player's
+ * corresponding board according to the game state. The player must be either
+ * "player" or "computer".
+ * @param {Game} game 
+ * @param {String} player 
+ */
+function refreshBoard(game,player) {
+    let board;
+    let tableRows;
+    switch(player) {
+        case player==="player":
+            board = game.getPlayerBoard().getBoard();
+            tableRows = game.getPlayerBoardElement().firstChild.children;
+            break;
+        case player==="computer":
+            board = game.getComputerBoard().getBoard();
+            tableRows = game.getComputerBoardElement().firstChild.children;
+            break;
+        default:
+            throw new Error("Invalid player");
+    }
+    for(let y=0;y<BOARDSIZE;y++) {
+        let tableCells = tableRows[y].children;
+        for(let x=0;x<BOARDSIZE;x++) {
+            let td = tableCells[x];
+            let cell = board[y][x];
+            switch(cell) {
+                case cell===0:
+                    td.setAttribute("class","empty unhit");
+                    break;
+                case cell===1:
+                    td.setAttribute("class","ship, unhit");
+                    break;
+                case cell===2:
+                    td.setAttribute("class","empty hit");
+                    break;
+                case cell===3:
+                    td.setAttribute("class","ship hit");
+                    break;
+                default:
+                    throw new Error("Invalid game board.");
+            }
+        }
+    }
+}
 
 function initializeGame() {
     contentDiv.replaceChildren();
-    const playerBoard = createGameboardElement(BOARDSIZE);
-    playerBoard.setAttribute("class","gameboard playerBoard");
+    const playerBoard = Gameboard.createGameboard(BOARDSIZE);
+    const playerBoardElement = createGameboardElement(BOARDSIZE);
+    playerBoardElement.setAttribute("class","gameboard playerBoard");
 
-    const computerBoard = createGameboardElement(BOARDSIZE);
-    computerBoard.setAttribute("class","gameboard computerBoard");
+    const computerBoard = Gameboard.createGameboard(BOARDSIZE);
+    const computerBoardElement = createGameboardElement(BOARDSIZE);
+    computerBoardElement.setAttribute("class","gameboard computerBoard");
 
-    contentDiv.appendChild(playerBoard);
-    contentDiv.appendChild(computerBoard);
+    contentDiv.appendChild(playerBoardElement);
+    contentDiv.appendChild(computerBoardElement);
+
+    return {
+        playerBoard:playerBoard,
+        playerBoardElement:playerBoardElement,
+        computerBoard:computerBoard,
+        computerBoardElement:computerBoardElement
+    }
 }
 
 
